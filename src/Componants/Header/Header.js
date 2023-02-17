b321d7b0d852c8aa67c62b4109b8679ae391a350
sidparabhavya84 <sidparabhavya84@gmail.com>
2023-02-17 22:59:24 +0530

    Portfolio

---
+++ src/Componants/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+            <header id="header" className="d-flex flex-column justify-content-center">
+                <nav id="navbar" className="navbar nav-menu">
+                    <ul>
+                        <li><a href="#hero" className="nav-link scrollto active"><i className="bx bx-home" /> <span>Home</span></a></li>
+                        <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user" /> <span>About</span></a></li>
+                        <li><a href="#resume" className="nav-link scrollto"><i className="bx bx-file-blank" /> <span>Resume</span></a></li>
+                        <li><a href="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content" /> <span>Portfolio</span></a></li>
+                        <li><a href="#services" className="nav-link scrollto"><i className="bx bx-server" /> <span>Services</span></a></li>
+                        <li><a href="#contact" className="nav-link scrollto"><i className="bx bx-envelope" /> <span>Contact</span></a></li>
+                    </ul>
+                </nav>{/* .nav-menu */}
+            </header>
+
+        </>
+    )
+}
+
+export default Header
